21c9c0ab311cd4022863aed1162db2948358cd7e
pine-hill <mat46.cutter.no9@gmail.com>
2020-09-02 16:49:10 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,16 @@ class PostsController < ApplicationController
     #post = Post.new(content: params[:content])
     #post.saveでも可
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
