d4937da1a782fc587e61138c305a388a2154fb7e
pine-hill <mat46.cutter.no9@gmail.com>
2020-09-02 15:30:57 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,13 @@
 class PostsController < ApplicationController
   def index #indexアクションを定義した
-    @posts = Post.all
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     #contentはテーブルのカラム名
     #paramsはリクエストパラメータ→contentが格納されている
     Post.create(content: params[:content])
+    redirect_to action: :index
     #post = Post.new(content: params[:content])
     #post.saveでも可
   end
